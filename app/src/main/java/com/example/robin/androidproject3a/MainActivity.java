829931ac37d26dacb0e908e5c5cca86650220954
roppe546 <roppe546@gmail.com>
2015-12-07 15:59:17 +0100

    Add preliminary code for accelerometer sensing.

--- app/src/main/java/com/example/robin/androidproject3a/MainActivity.java
+++ app/src/main/java/com/example/robin/androidproject3a/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.robin.androidproject3a;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    SensorManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        manager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Sensor accelerometer = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        if (accelerometer != null) {
+            // Success! There's an accelerometer
+            Log.i("SENSOR", "Sensors found.");
+
+            manager.registerListener(sensorEventListener, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        else {
+            Log.i("SENSOR", "No sensor found.");
+        }
     }
+
+    private final SensorEventListener sensorEventListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            double x = event.values[0];
+            double y = event.values[1];
+            double z = event.values[2];
+            long time = event.timestamp;
+
+            Log.i("SENSE", "x: " + x + ", y: " + y + ", z: " + z + ", at " + time);
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            return;
+        }
+    };
 }
